172a8b604c3d930d85155e3b5453890dfa66c7d1
Peter <poconn4@gmail.com>
2011-12-04 18:55:14 +0100

    woooo

---
+++ HomeOwner.m
@@ -0,0 +1,39 @@
+classdef HomeOwner < handle
+    
+    properties
+        
+        cash;
+        
+        H;  % Array of homes
+        
+        
+        
+        
+        
+    end
+    
+    methods
+        
+        
+        function buy(A)
+        
+        
+        
+        end
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    end
+    
+    
+    
+    
+    
+    
+end
